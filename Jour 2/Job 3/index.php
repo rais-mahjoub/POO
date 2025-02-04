3f05366eccf8c11e262042196a0f7ce95c1bfa07
rais-mahjoub <rais.mahjoub@laplateforme.io>
2025-02-04 12:01:26 +0100

    feat: Jour 2/Job 3

--- Jour 2/Job 3/index.php
+++ Jour 2/Job 3/index.php
@@ -4,11 +4,13 @@ class Livre {
     private $titre;
     private $auteur;
     private int $nbePages;
+    private bool $dispo;
 
     public function __construct($titre, $auteur, $nbePages) {
         $this->titre = $titre;
         $this->auteur = $auteur;
         $this->nbePages = $nbePages;
+        $this->dispo = true;
     }
 
     public function getTitre() {
@@ -38,6 +40,30 @@ class Livre {
             echo"Nombre invalide ($nbePages)<br>";
         }
     }
+
+    public function verification() {
+        if ($this->dispo) {
+            return "Ce livre est disponible. <br>";
+        } else {
+            return "Ce livre n'est pas disponible. <br>";
+        }
+    }
+
+    public function emprunter() {
+        if ($this->dispo) {
+            $this->dispo = false;
+        } else {
+            echo"Impossible d'emprunter un livre indisponible.<br>"; 
+        }
+    }
+
+    public function rendre() {
+        if (!$this->dispo) {
+            $this->dispo = true;
+        } else {
+            echo "Vous ne pouvez pas rendre un livre que vous n'avez pas emprunter.<br>";
+        }
+    }
 }
 
 $livre = new Livre("Le livre de la jungle", "Mowgli", 271);
@@ -48,4 +74,18 @@ echo $livre->getNbePages()."<br>";
 $livre->setNbePages(331.5);
 echo $livre->getNbePages()."<br>";
 $livre->setNbePages(289);
-echo $livre->getNbePages()."<br>";
+echo $livre->getNbePages()."<br><br>";
+
+echo $livre->verification();
+$livre-> emprunter();
+echo $livre->verification();
+$livre-> rendre();
+echo $livre->verification();
+$livre->rendre();
+echo $livre->verification();
+$livre-> emprunter();
+echo $livre->verification();
+$livre-> emprunter();
+echo $livre->verification();
+$livre->rendre();
+echo $livre->verification();
